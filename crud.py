5eba17f4e6a8917982fcfda242dafe0c3c85b82b
denizkarakus123 <dkarakus2323@gmail.com>
2024-11-22 21:45:15 -0500

    added some of the database stuff

---
+++ crud.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from . import models
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def create_user(db: Session, username: str, hashed_password: str):
+    db_user = models.User(username=username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
